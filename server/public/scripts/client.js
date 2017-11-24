77edd0a7b19e62ff507917bd1f47fc0e58c959fe
Stephanie Ratanas <stephanieratanas@gmail.com>
2017-11-24 15:56:09 -0600

    got task delete and move button to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,13 +2,17 @@ console.log('client.js is loaded');
 
 $(document).ready(function(){
     console.log('JQ');
+    // getTasks();
 $('#addTask').on('click', function(){
     var taskObject = {
-        task: $('input').val()
+        task: $('input').val(),
+    
     }
     addTask(taskObject);
+ 
 
 $('#viewTasks').on('click', '.deleteButton', deleteTask)
+$('#viewTasks').on('click', '.completedButton', updateToComplete);
 });  
 
 
@@ -37,20 +41,30 @@ function getTasks(){
       success: function(response){
         console.log( 'got some tasks ', response);
         $('#viewTasks').empty();
+        $('input').val('');
         
           for (var i = 0; i < response.length; i++){
               var task = response[i];
-              var $newTask = $('<tr>' + '<td>' + task.task + '</td>'+ '</tr>');
+              var $newTask = $('<tr>' + '<td id = "tableId">' + task.task + '</td>'+ '</tr>');
             
+
+              if (task.completed !== 'Yes'){
+                
+                var $completedButton = $('<button class = "completedButton">completed</button>')
+                $newTask.append($completedButton); //$completedButton is now appended INSIDE $newShoeItem
+                $completedButton.data('id', task.id)}
+              //create and append complete button
+            //   var $completedButton = $('<button class = "completedButton">Completed</button>');
+            //   $newTask.append($completedButton);
+            //   $completedButton.data('id', task.id);
+            //   $completedButton.data('task', task.task);
+
               //create and appending delete button
               var $deleteTaskButton = $('<button class = "deleteButton">Delete</button>');
               $newTask.append($deleteTaskButton);
               $deleteTaskButton.data('id',task.id);
 
-              //create and append complete button
-              var $completedButton = $('<button class = "completedButton">Completed</button>');
-              $newTask.append($completedButton);
-              $completedButton.data('id', task.id);
+
 
               //append tasks to view tasks table
               $('#viewTasks').append($newTask);
@@ -63,6 +77,7 @@ function getTasks(){
   }; // end getTasks
 
   function deleteTask(){
+    console.log('in deleteTask');
     console.log($(this).data());
     var taskToRemove = $(this).data().id;
     console.log('Clicked delete, task id was ', taskToRemove);
@@ -77,4 +92,40 @@ function getTasks(){
     })
       
   } //end delete tasks
+
+  function updateToComplete(){
+    console.log($(this).data());
+    var taskToSetComplete = $(this).data().id;
+    var taskToMove = $(this).data().task;
+    console.log('taskToMove', $(this).data().task);
+    
+    console.log('Task completed, the task ID was, ', taskToSetComplete);
+    var completed =  $(this).parent().children('#tableId')
+    $.ajax({
+      method: 'PUT',
+      url:'/toDo/' + taskToSetComplete,
+      data: completed,
+      success: function(response){
+        console.log('Task complete success', response);
+        //need to add CSS to change to green, possibly move to bottom?
+
+        // $('#tableId').css('background-color','green');       
+        // $('#tableId').remove(taskToMove);
+        // $(this).remove('.completedButton')
+        // $(this).remove(completed)
+        $('#viewTasks').remove('.completedButton')
+        $('#viewComplete').append(completed).css('background-color','green');
+        // getTasks();
+      }
+    })
+
+  } // end updateToComplete
+
+
+  function deleteAndAppend (){
+    $(this).remove(taskToMove);
+    $(this).remove('.deleteButton')
+    $(this).remove(completed)
+    $('#viewComplete').append(taskToMove).css('background-color','green');
+  }
 
--- server/routes/toDo.js
+++ server/routes/toDo.js
@@ -67,4 +67,35 @@ router.get('/', function(req, res){
     })
   });//end router delete
 
+
+  router.put('/:id', function (req, res) { //params = whatever is after the whack on a website (after/shoes/)
+    var taskToSetComplete = req.params.id //whatever is at the end set it equal to the above property
+    pool.connect(function (errorConnectingToDatabase, client, done) { 
+        if (errorConnectingToDatabase) {
+            //there was an error connecting to the database     
+            console.log('Error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);//sending something went wrong, you don't need to know the details
+
+        } else {
+            //we connected to the database!!!
+            //now we are going to GET things from the DB .gets should have selects in them
+            client.query(`UPDATE tasks SET completed = 'Yes' WHERE id=$1`, [taskToSetComplete], function (errorMakingQuery, result) { // this is the callback function we make after our query runs
+                //can't put req.body ^ here or you'll get hacked    
+                done(); //it just ran, close the connection we don't need it anymore, put it back in the pool
+                if (errorMakingQuery) {
+                    // Query failed. Did you test it in postico?
+                    //log the error
+                    console.log('Error making query', errorMakingQuery);
+                    res.sendStatus(500);
+
+                } else {
+                    res.sendStatus(200); //this is our array of objects
+                }
+            }) //test queries in postico first and then copy and paste, semicolon included
+
+        }
+    });
+}); // end router.put
+
+
 module.exports = router;
